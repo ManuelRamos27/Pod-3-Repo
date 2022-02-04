a5c0db402d35247b43c340a3efb98035edeb1863
Ulysses Ware <utware007@gmail.com>
2022-02-04 16:06:03 -0500

    Challenge_01

---
+++ Ulysses W/02.04.22 re first_python_challenge.py
@@ -0,0 +1,40 @@
+from email.quoprimime import body_check
+from http.client import MOVED_PERMANENTLY
+
+
+print('1: Describe what is happening below by adding comments before each line')
+# assigning the string books to the variable box_1.  box_1 = 'books'
+# assigning the string clothes to the variable box_2.  box_2 = 'clothes'
+# assigning the string plants to the variable box_3. box_3 = 'plants'
+# assigning the string kitchen stuff to the variable box_4.  box_4 = 'kitchen stuff'
+
+# printing on one line the print string between the quotes, separated by a space  print('2: Print the variables box_1, box_2, box_3, box_4')
+
+print('3.1: Declare a variable with the name "address", assign it any street name you like')
+
+address = "Park Ave"
+
+print('3.2: Print the address variable')
+
+print("Park Ave")
+
+
+print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
+
+box_2 = "Money" 
+
+box_4 = "Billionaire"
+
+print("books", "clothes",  "plants", "Money", "Billionaire")
+
+
+print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
+#completion message = 'Completed the first Python challenge!
+
+completion_message = "Completed the first Python challenge!"
+
+print('5.2: Turn the comment below back into regular Python code')
+#print(completion_message)
+
+print("completion_message")
+
