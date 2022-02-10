8a96ebb6c227478f8bff0fb96a9e05aeb76d7d88
Anthony Lopez <anthonylopeznyc@gmail.com>
2022-02-10 16:32:15 -0500

    working on temperature challenge

--- Anthony L/temperature.py
+++ Anthony L/temperature.py
@@ -1,9 +1,30 @@
 #temperature challenge
 
-celsius_100 = 100
-celsius_100 - 32 * 5/9
+from calendar import c
+#temperature challenge
+
+
+# 1. Convert a temperature of 100 degrees fahrenheit to celsius
+#     * Save this to a variable called `celsius_100`, and use `print()` to print out the value
+#     * Is the resulting temperature you get an integer or float? How do you know?
+
+celsius_100 = 100 #1
+
+
+print(celsius_100) #1
+print(type(celsius_100)) #1
+
+input ("En")
+
+#the resulting temperature is an integer because it does not have a decimal point. running the (type) function also reveals the temperature is <class 'int'>
+
+
+# 2. Convert a temperature of 0 degrees fahrenheit to celsius
+#     * Save this to a variable called `celsius_0`, and use `print()` to print out the value
+
+
+#celsius_100 - 32 * 5/9
 
 
 
 
-#resulting temperature number is a float because there are no decimals
