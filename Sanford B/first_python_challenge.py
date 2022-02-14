06aaa46d5786befca53994cfb5f73de96ad6681e
Sanford Barber <Sanfordbarber30@yahoo.com>
2022-02-14 17:01:39 -0500

    test push

---
+++ Sanford B/first_python_challenge.py
@@ -0,0 +1,19 @@
+print('1: Describe what is happening below by adding comments before each line')
+box_1 = 'books'
+box_2 = 'clothes'
+box_3 = 'plants'
+box_4 = 'kitchen stuff'
+
+print('2: Print the variables box_1, box_2, box_3, box_4')
+
+print('3.1: Declare a variable with the name "address", assign it any street name you like')
+
+print('3.2: Print the address variable')
+
+print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
+
+print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
+#completion message = 'Completed the first Python challenge!
+
+print('5.2: Turn the comment below back into regular Python code')
+#print(completion_message)
