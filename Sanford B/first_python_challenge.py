58880bbd5f5b9d69608f03952ea3ee14784509a6
Sanford Barber <Sanfordbarber30@yahoo.com>
2022-02-17 23:49:49 -0500

    stocks_challenge.py

--- Sanford B/first_python_challenge.py
+++ Sanford B/first_python_challenge.py
@@ -1,19 +1,33 @@
-print('1: Describe what is happening below by adding comments before each line')
-box_1 = 'books'
-box_2 = 'clothes'
-box_3 = 'plants'
-box_4 = 'kitchen stuff'
+print('1.1: Create a variable called `meal`, and save a string describing what you had for lunch in it')
+meal= "I had chicken"
 
-print('2: Print the variables box_1, box_2, box_3, box_4')
+print('1.2: Print the meal variable')
+print=(meal)
 
-print('3.1: Declare a variable with the name "address", assign it any street name you like')
+print('1.3: Update the meal variable to be a string describing what you want for dinner. print it out again')
 
-print('3.2: Print the address variable')
 
-print('4: Reassign variables box_2 and box_4 with some other text and print box_1, box_2, box_3, box_4 again')
+print('2: How old is Google?')
+# 2.1: Google was founded in 1993. The current year is 2022. Create a variable called google_age, and use subraction
+# to figure out how old Google is
+# ex: my_age = current_year - birth_year
 
-print('5.1: The line of code below is commented out because it produces many SyntaxErrors. Fix the problem and turn the comment back into regular Python code')
+
+# 2.2: Print out a sentence about Google's age. Make sure to include your variable in the f-string!
+
+
+
+# 2.3 How many _months_ old is Google? Create a new variable google_age_months, and use multiplication to figure it out,
+# then print the info.
+# (Assume 12 months for each year, you don't need to check which month they started)
+
+
+
+print('3.1: The line of code below is commented out because it produces many SyntaxErrors.')
+print('Fix the problem and turn the comment back into regular Python code')
 #completion message = 'Completed the first Python challenge!
 
-print('5.2: Turn the comment below back into regular Python code')
+# 3.2 What were the syntax errors that you fixed? print out a quick explanation of each one.
+
+print('3.3: Turn the comment below back into regular Python code')
 #print(completion_message)
