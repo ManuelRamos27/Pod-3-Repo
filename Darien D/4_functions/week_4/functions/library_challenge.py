3e7eb43311195f4d0cb21928d0a68d69ed4edcde
Darien O. Davis <dariendarien03@gmail.com>
2022-02-22 22:11:23 -0500

    Completed Library Challenge

--- Darien D/4_functions/week_4/functions/library_challenge.py
+++ Darien D/4_functions/week_4/functions/library_challenge.py
@@ -1,3 +1,8 @@
+from http.client import FOUND
+from tabnanny import check
+from turtle import title
+
+
 print('Question 1:')
 # You are working on a library management system, here are the list books at the library
 books = ['MY OWN WORDS', 'WHITE FRAGILITY', 'THE BODY KEEPS THE SCORE', 'SO YOU WANT TO TALK ABOUT RACE', 'STAMPED FROM THE BEGINNING', 'JUST MERCY', 'BORN A CRIME',
@@ -12,44 +17,54 @@ print(type(books))
 # Create a function 'available_books' to print the books list
 # Parameters: Not needed for this function
 # Return: Not needed for this function
-
+def avaliable_books():
+    print(books)
 # 1.2
 # Run the 'available_books' function
-
+avaliable_books()
 # 1.3
 # Create a function 'check_out' that removes a book from the books list
 # Parameters: book (string)
 # Return: Not needed for this function
-
+def check_out(book):
+    books.remove(book)
+    
 # 1.4
 # Check out 'SAPIENS' using the check_out function
-
+check_out('SAPIENS')
 # Bonus: Run available_books function again to see if the book was checked out
-
+avaliable_books()
 # 1.5
 # Create a function 'check_in' that adds a book to the books list
 # Parameters: book (string)
 # Return: Not needed for this function
+def check_in(book):
+    books.append(book)
 
 # 1.6
 # Check in 'SAPIENS' using the check_in function
-
+check_in("SAPIENS")
 # Bonus: Run available_books function to see if the book was checked in
-
+avaliable_books()
 # 1.7
 # Create a function 'search_by_name' that prints 'Available' if exists in books list, 'Not Available' if it doesn't.
 # Parameters: book (string)
 # Return: Not needed for this function
+def search_by_name(book):
+    if book in books:
+        print("\n Avaliable\n")
+    else:
+        print("\nUnavaliable\n")
 
 # 1.8
 # Search for the book 'JUST MERCY'
+search_by_name('JUST MERCY')
 
-print()
 
 print('Question 2')
 # Here's the same list of books, with additional details
 books_with_details = [
-    {
+   {
         'title': 'MY OWN WORDS',
         'author': 'Ruth Bader Ginsburg with Mary Hartnett and Wendy W Williams',
         'description': 'A collection of articles and speeches by the Supreme Court justice.'
@@ -132,22 +147,37 @@ books_with_details = [
 ]
 # 2.0
 # Describe the structure of the data in books_with_details. What types of data are nested within others? How do you know?
-
+#list[dict[str, str]]
 # 2.1
 # Create a function 'count_books' that returns the number of books in the books_with_details list
 # Parameters: Not needed for this function
 # Return: number of books (integer)
-
+def count_books():
+    return len(books_with_details)
 # 2.2
 # Check the number of books available in the books list using the count_books function
 
+num_of_books= count_books()
+print(num_of_books)
 # 2.3
 # Create a function 'search_by_author' that returns the titles of books by an author
 # Parameters - author (string)
 # Return - author's books (list of strings)
 # Hint - You will need a for loop, if statement, .append() for this solution!
 
+def search_by_author(author):
+    found_author=[]
+    for i in range(len(books_with_details)):
+     if (books_with_details[i]['author']) == author:
+        found_author.append(books_with_details[i]['title'])
+    else:
+        
+        return found_author
+    
+  
 
-# 2.4
-# Search for book titles by the author 'Timothy Snyder' using the search_by_author function
+# # 2.4
+# # Search for book titles by the author 'Timothy Snyder' using the search_by_author function
+
+print(search_by_author('Timothy Snyder'))
 
