989c211e8078f0a2ad338af5a77c703ff1a0874a
Darien O. Davis <dariendarien03@gmail.com>
2022-03-14 10:51:25 -0400

    BookClass

--- Darien D/1_oop/book_class.py
+++ Darien D/1_oop/book_class.py
@@ -1,5 +1,10 @@
 
+from turtle import title
+
+
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
+        return str(self.title)
